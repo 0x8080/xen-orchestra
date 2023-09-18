e25397c4de091747c4e62ea3db4d209bcf0f1c5d
0x8080 <shadowbrony33@gmail.com>
2023-09-18 14:52:52 -0600

    Update xoa-plans.js

--- packages/xo-web/src/common/xoa-plans.js
+++ packages/xo-web/src/common/xoa-plans.js
@@ -31,19 +31,7 @@ export const productId2Plan = {
 }
 
 export const getXoaPlan = (plan = +process.env.XOA_PLAN) => {
-  switch (+plan) {
-    case 1:
-      return FREE
-    case 2:
-      return STARTER
-    case 3:
-      return ENTERPRISE
-    case 4:
-      return PREMIUM
-    case 5:
-      return SOURCES
-    default:
-      return UNKNOWN
+    return ENTERPRISE
   }
 }
 
